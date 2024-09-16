0ea8a5938cb9c3ea3cb0a746fa5f172a5e0772cd
Dipanshu-03malik <malikdipanshu88@gmail.com>
2024-09-16 16:40:32 +0530

    updTED

--- codeblock/src/MyTree/client.java
+++ codeblock/src/MyTree/client.java
@@ -1,7 +1,6 @@
 package MyTree;
-<<<<<<< HEAD
 import java.util.*;
-public class Client {
+public class client {
 	static int max=0;
 	
 	public static int Count (TreeNode root) {
@@ -198,50 +197,5 @@ public class Client {
 		}
 		
 	}
-
-=======
-
-public class client {
-    public static void preorder(TreeNode root)
-    {
-        if(root==null)
-        {
-            return;
-        }
-        System.out.print(root.data);
-
-        preorder(root.left);
-        preorder(root.right);
-    }
-    public static void inorder(TreeNode root) {
-        if(root==null)
-        {
-            return;
-        }
-        inorder(root.left);
-        System.out.println(root.data);
-        inorder(root.right);
-        
-    }
-    public static void postorder(TreeNode root) {
-        if(root==null)
-        {
-            return;
-        }
-        inorder(root.left);
-        inorder(root.right);
-        System.out.print(root.data);
-    }
-    public static void main(String [] args)
-    {
-        //1 2 -1 -1 3 4 6 -1 -1 7 -1 -1 5 -1 -1
-        Tree t=new Tree();
-        System.out.print("preorder");
-        preorder(t.root);
-        System.out.println("\n inorder");
-        inorder(t.root);
-        System.err.println("\n postorder");
-        postorder(t.root);
-    }
->>>>>>> ba3eb9180f5359fc21e8e1cab9b854a5b47e59ca
 }
+
